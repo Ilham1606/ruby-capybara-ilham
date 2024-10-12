e53909c50171a5201797ae3439a90b585e933efd
Ilham1606 <16ilhamapriansyah@gmail.com>
2024-10-12 12:13:04 +0700

    Sync all pertemuan

--- features/step_definitions/pertemuan_3_steps.rb
+++ features/step_definitions/pertemuan_3_steps.rb
@@ -14,3 +14,14 @@ Then('I can see remove button for each products on the inventory page') do
     step 'I can see remove button for the product "' + row['product_name'] + '" on the inventory page'
   end
 end
+
+
+When('[READ FILE] I enter username as {string} on the login page') do |username|
+  username_value = get_data_test(username)
+  step 'I enter username as "' + username_value + '" on the login page'
+end
+
+When('[READ FILE] I enter password as {string} on the login page') do |password|
+  password_value = get_data_test(password)
+  step 'I enter password as "' + password_value + '" on the login page'
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,8 @@ require 'dotenv'
 require 'selenium-webdriver'
 require 'yaml'
 
+Dotenv.load
+
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
@@ -19,4 +21,16 @@ end
 Capybara.configure do |config|
   config.default_driver = :chrome
   config.default_max_wait_time = 30
+end
+
+# single env
+def get_data_test_single_env(key)
+  file = YAML.load_file("features/support/data/data-test.yml")
+  return file[key]
+end
+
+# multy env
+def get_data_test(key)
+  file = YAML.load_file("features/support/data/data-test-#{ ENV['TARGET'].downcase }.yml")
+  return file[key]
 end
